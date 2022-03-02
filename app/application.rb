b95d8f5749656743a774e554e9f5dc1201bd5eed
Julio Aguilera Jr <julioaguilera337@gmail.com>
2022-03-01 18:33:08 -0600

    Added Numbers

--- app/application.rb
+++ app/application.rb
@@ -2,8 +2,24 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    
+    num_1 = Kernel.round(1..20)
+    num_2 = Kernel.round(1..20)
+    num_3 = Kernel.round(1..20)
+
+    resp.write "#{num_1}\n"
+    resp.write "#{num_2}\n"
+    resp.write "#{num_3}\n"
+    
+    if num_1 == num_2 && num_2 == num_3
+      resp.Write "You Win"
+    else
+      resp.Write "You loose"
+    end
+    
     resp.finish
   end
+    
+  
 
 end
